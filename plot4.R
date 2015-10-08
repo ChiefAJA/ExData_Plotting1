a9ab84d8b20cffe38aeab8d1e9cd1d3f4ff73740
ChiefAJA <chief.agith@gmail.com>
2015-10-07 21:08:23 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,6 +1,6 @@
 png(file = "plot4.png") #open file
 
-#load data for household power consumption 
+#load data for household power consumption  - assumes household_power_consumption.txt is in a folder called Data 
 data <- tbl_df(read.table("Data/household_power_consumption.txt", header = TRUE, sep=";", na.strings = c("?","")))
 
 #adjust Date and Time variables to more usable class
